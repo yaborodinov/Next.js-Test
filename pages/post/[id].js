35addefaad347a05609bc4607e67761193bb67d7
Alexandr <yaborodinov@gmail.com>
2021-09-23 23:05:17 +0300

    added DB, added post page in dinamic

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,14 +1,34 @@
 import { useRouter } from 'next/router'
+import axios from 'axios'
+import Link from 'next/link'
+import classes from '../../styles/btn.module.scss'
 
 import {MainLoyout} from '../../components/MainLoyout'
 
-const Post = () => {
-  const router = useRouter()
+const Post = ({data}) => {
+  console.log(data);
   return (
     <MainLoyout>
-      <h1>Post { router.query.id}</h1>
+      <h1>{data.title}</h1>
+      <br />
+      <p>{data.body}</p>
+      <Link href="/posts">
+        <a>
+          <button className={classes.btn}>Back to Posts</button>
+        </a>
+      </Link>
     </MainLoyout>
   )
 }
 
 export default Post
+
+Post.getInitialProps = async (ctx) => {
+
+  const response = await axios.get(`http://localhost:4200/posts/${ctx.query.id}`)
+  const data = response.data
+  
+  return {
+    data
+  }
+}
--- pages/posts.js
+++ pages/posts.js
@@ -1,15 +1,39 @@
 import Link from 'next/link'
+import axios from 'axios'
 
 import {MainLoyout} from '../components/MainLoyout'
 
-const Posts = () => (
-  <MainLoyout>
-    <h1>Posts page</h1>
-    <Link href="/">
-      <a><button>Main Page</button></a>
-    </Link>
-  </MainLoyout>
-)
+const Posts = ({ data }) => {
+
+  return(
+    <MainLoyout>
+        <h1>Posts page</h1>
+        <ul>
+          {data.map((post) => (
+            <li key={post.id}>
+              <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                <a>
+                   {post.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      <Link href="/">
+        <a><button>Main Page</button></a>
+      </Link>
+    </MainLoyout>
+  )
+}
 
 
 export default Posts
+
+Posts.getInitialProps = async () => {
+  const response = await axios.get('http://localhost:4200/posts')
+  const data = response.data
+
+  return {
+    data
+  }
+}
