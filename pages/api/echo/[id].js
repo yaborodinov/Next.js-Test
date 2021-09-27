60dc4db8ff3f4972d0d0877df408b6049651b009
Alexandr <yaborodinov@gmail.com>
2021-09-27 20:41:08 +0300

    added entrypoint

---
+++ pages/api/echo/[id].js
@@ -0,0 +1,6 @@
+export default function getById(req, res) {
+  // res.statusCode = 200
+  // res.setHeader('Content-Type', 'application/json')
+  // res.end(req.query.id)
+  res.json({ message: req.query.id })
+}
